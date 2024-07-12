ef46064347f8000b3ed483fb35f82859c6456af2
Felix Teng <fsmunozteng@gmail.com>
2024-07-11 21:00:25 -0700

    updated seed.js that initiates seeding for npm run seed

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,62 +1,31 @@
-// Importing the mongoose library for MongoDB connection
-const mongoose = require('mongoose');
+const db = require('../config/connection');
+const { Customer } = require('../models');
+// const { Product } = require('../models');
+// const { TransactionMain } = require('../models');
+// const { TransactionDetail } = require('../models');
+const customerSeeds = require('./customerData.json');
+// const productSeeds = require('./productData.json');
+// const transactionDetailSeeds = require('./transactionDetailData.json');
+// const transactionMainSeeds = require('./transactionMainData.json');
+const cleanDB = require('./cleanDB');
 
-// Importing models for database operations
-const { Customer, Product, TransactionMain, TransactionDetail } = require('../models');
-
-// Importing seed data from JSON files
-const customerData = require('./customerData.json');
-const productData = require('./productData.json');
-const transactionMainData = require('./transactionMainData.json');
-const transactionDetailData = require('./transactionDetailData.json');
-
-// Connecting to MongoDB using environment variable or default local database
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TeamGoat_db', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-
-// Asynchronous function to seed the database
-const seedDatabase = async () => {
+db.once('open', async () => {
   try {
-    // Deleting existing data in collections to avoid duplicates
-    await Customer.deleteMany({});
-    await Product.deleteMany({});
-    await TransactionMain.deleteMany({});
-    await TransactionDetail.deleteMany({});
 
-    // Inserting seed data into collections
-    const customers = await Customer.insertMany(customerData);
-    const products = await Product.insertMany(productData);
+    await cleanDB('Customer', 'customers');
 
-    // Mapping transaction main data to associate with customers
-    const transactionMainEntries = transactionMainData.map((transaction, index) => {
-      transaction.customer_id = customers[index % customers.length]._id;
-      return transaction;
-    });
+    await Customer.create(customerSeeds);
+    // await Product.create(productSeeds);
+    // await TransactionMain.create(transactionDetailSeeds);
+    // await TransactionDetail.create(transactionMainSeeds);
 
-    // Inserting mapped transaction main data
-    const transactionMains = await TransactionMain.insertMany(transactionMainEntries);
-
-    // Mapping transaction detail data to associate with transactions and products
-    const transactionDetailEntries = transactionDetailData.map((detail, index) => {
-      detail.transaction_id = transactionMains[index % transactionMains.length]._id;
-      detail.product_id = products[index % products.length]._id;
-      return detail;
-    });
-
-    // Inserting mapped transaction detail data
-    await TransactionDetail.insertMany(transactionDetailEntries);
-
-    // Logging success message and exiting the process
-    console.log('Database seeded!');
-    process.exit(0);
   } catch (err) {
-    // Logging error message and exiting the process with failure code
     console.error(err);
     process.exit(1);
   }
-};
 
-// Calling the seed function to execute the seeding process
-seedDatabase();
+  console.log('Database seeded!');
+  process.exit(0);
+});
+
+
