87a1210bd74ddecaa0e0c92b2aebc0929792a856
GW <garykachunwu@gmail.com>
2024-07-11 19:50:37 -0700

    class  to class name on jsx files

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -55,14 +55,14 @@ function Navbar() {
                     <div>
                     {loggedIn ? (
                     <button className="mx-2 fs-5 nav-link" id="logout">
-                        <svg className="mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <svg className="mb-1 bi bi-person-fill" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                         </svg>
                         Logout
                     </button>
                     ) : (
                     <a className="mx-2 fs-5 nav-link" href="/login">
-                        <svg className="mb-1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                        <svg className="mb-1 bi bi-person-fill" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 16 16">
                         <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
                         </svg>
                         Login
