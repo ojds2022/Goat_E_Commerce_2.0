3e17bf0d346797bbfe0d8ecfac5a1c6e7eb61cde
GW <garykachunwu@gmail.com>
2024-07-13 13:05:20 -0700

    finished shopping cart page, not server

--- server/models/Customer.js
+++ server/models/Customer.js
@@ -49,7 +49,7 @@ customerSchema.pre('save', async function (next) {
 });
 
 // Method to check if the provided password matches the hashed password
-customerSchema.methods.checkPassword = function (password) {
+customerSchema.methods.isCorrectPassword = function (password) {
   return bcrypt.compareSync(password, this.password);
 };
 
