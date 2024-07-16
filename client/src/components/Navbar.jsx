b9474c74fd0cdd70de8a80bce444a384efccd4ba
Felix Teng <fsmunozteng@gmail.com>
2024-07-15 19:26:55 -0700

    updated styling for login and sign up button for changing background color

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -88,10 +88,10 @@ function Navbar() {
                                 </>
                                 ) : (
                                 <>
-                                <Link className="btn btn-lg btn-info m-2" to="/login">
+                                <Link className="btn btn-lg m-2" to="/login">
                                     Login
                                 </Link>
-                                <Link className="btn btn-lg btn-light m-2" to="/signup">
+                                <Link className="btn btn-lg m-2" to="/signup">
                                     Signup
                                 </Link>
                             </>
