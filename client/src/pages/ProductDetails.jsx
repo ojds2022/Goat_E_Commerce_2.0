66f85ac400e1fedcf2fe0b386a4af8dcae92cec6
RApostolJR <romuloapostol@gmail.com>
2024-07-16 21:06:15 -0700

    small fixes to ProductDetails

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
+import { ADD_TRANSACTION_MAIN } from "../utils/mutations";
+import { useMutation } from '@apollo/client';
 
 
-const loggedIn = true;
+const loggedIn = false;
 
 //const customer_id = 12345;
 
 export default function ProductDetails() {
     const location = useLocation();
     const navigate = useNavigate();
+    const [addTransactionMain, {data, loading, error}] = useMutation(ADD_TRANSACTION_MAIN);
     
     useEffect(() => {
         if (!location.state) {
@@ -22,8 +25,13 @@ export default function ProductDetails() {
 
     const { product } = location.state;
 
+    // const handleAddToCart = async () => {
+    //     if (!loggedIn) {
+
+    //     }
+    // }
+
     const getQuantity = () => {
-        //const productQuantity = document.querySelector('#product-quantity').value;
         navigate(`/shoppingCart`);
     }
 
@@ -48,13 +56,8 @@ export default function ProductDetails() {
                 <div className="product-details">
                     <h4 id="product-name">{product.product_name}</h4>
                     <h5 id="product-description">{product.product_description}</h5>
-                    <h6 id="product-price">Login to view price</h6>
-                    <div className="product-bought mx-auto" style={{width: "25%"}}>
-                        <input type="number" style={{width: "4em"}} id="product-quantity" placeholder="0"/>
-                        <button className="btn btn-warning btn-sm mt-2">ADD</button>
-                    </div>
+                    <h6 id="product-price"><strong>LOGIN TO VIEW PRICE</strong></h6>
                 </div>
-                {/* <p className="d-none" id="customerID">{customer_id}</p> */}
             </section>
         )}
         </>
