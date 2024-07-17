f51e231fb2d2b0f5be3749f67aef348d3f7080bf
RApostolJR <romuloapostol@gmail.com>
2024-07-16 21:23:39 -0700

    added more fixes to ProductDetails.jsx

--- client/src/pages/ProductDetails.jsx
+++ client/src/pages/ProductDetails.jsx
@@ -2,15 +2,18 @@ import React, { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { ADD_TRANSACTION_MAIN } from "../utils/mutations";
 import { useMutation } from '@apollo/client';
+import Auth from '../utils/auth';
 
 
-const loggedIn = false;
+
+const loggedIn = true;
 
 //const customer_id = 12345;
 
 export default function ProductDetails() {
     const location = useLocation();
     const navigate = useNavigate();
+    
     const [addTransactionMain, {data, loading, error}] = useMutation(ADD_TRANSACTION_MAIN);
     
     useEffect(() => {
@@ -25,40 +28,51 @@ export default function ProductDetails() {
 
     const { product } = location.state;
 
-    // const handleAddToCart = async () => {
-    //     if (!loggedIn) {
-
-    //     }
-    // }
-
-    const getQuantity = () => {
+    const handleAddToCart = async () => {
         navigate(`/shoppingCart`);
+        try {
+            const customer = Auth.getProfile().data;
+            const customerId = customer.id;
+
+            const total = product.price;
+            const ordered = false;
+
+            const response = await addTransactionMain({
+                variables: { ordered, customer_id: customerId, total }
+            })
+        } catch (err) {
+            console.error(err);
+            alert('Error adding product to cart. Please try again later.');
+        }
     }
 
     return (
         <>
         {loggedIn ? (
-            <section className="product text-center">
-                <img src={product.product_url} alt={product.product_name} width="400" id="product-image" />
-                <div className="product-details">
-                    <h4 id="product-name">{product.product_name}</h4>
-                    <h5 id="product-description">{product.product_description}</h5>
-                    <h6 id="product-price">Cost: ${product.price}</h6>
-                    <div className="product-bought mx-auto" style={{width: "25%"}}>
-                        {/* <input type="number" style={{width: "4em"}} id="product-quantity" placeholder="0"/> */}
-                        <button onClick={getQuantity} className="btn btn-warning btn-sm mt-2">ADD</button>
+            <section className = "container">
+                { allProductData && (
+                    <section className="product text-center">
+                    <img src={product.product_url} alt={product.product_name} height="400" width="400" id="product-image" />
+                    <div className="product-details">
+                        <h4 id="product-name">{product.product_name}</h4>
+                        <h5 id="product-description">{product.product_description}</h5>
+                        <h6 id="product-price">Cost: ${product.price}</h6>
+                        <form className="product-bought mx-auto" style={{width: "25%"}}>
+                            <button onClick={handleAddToCart} className="btn btn-warning btn-sm mt-2">ADD</button>
+                        </form>
                     </div>
-                </div>
-            </section>
-        ) : (
-            <section className="product text-center">
-                <img src={product.product_url} alt={product.product_name} height="400" width="400" id="product-image" />
-                <div className="product-details">
-                    <h4 id="product-name">{product.product_name}</h4>
-                    <h5 id="product-description">{product.product_description}</h5>
-                    <h6 id="product-price"><strong>LOGIN TO VIEW PRICE</strong></h6>
-                </div>
-            </section>
+                    </section>  
+                )}
+            </section> 
+                ) : (
+                <section className="product text-center">
+                    <img src={product.product_url} alt={product.product_name} height="400" width="400" id="product-image" />
+                    <div className="product-details">
+                        <h4 id="product-name">{product.product_name}</h4>
+                        <h5 id="product-description">{product.product_description}</h5>
+                        <h6 id="product-price"><strong>LOGIN TO VIEW PRICE</strong></h6>
+                    </div>
+                </section>
         )}
         </>
     );
