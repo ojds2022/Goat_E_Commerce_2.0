ca57d66fa8351a2f245335b9b8ad1dbd13c68aba
ojds2022 <odiazstrandberg@gmail.com>
2024-07-16 19:03:20 -0700

    feat: product page, product detail page

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TeamGoat_db');
-
-module.exports = mongoose.connection;
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TeamGoat_db', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true, // This is for replacing deprecated ensureIndex
+    useFindAndModify: false // This is for replacing deprecated findAndModify
+  });
+  
+  module.exports = mongoose.connection
